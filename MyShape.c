7b962bd990e63524538663d8a1ac25d25b21a754
Olteanu Teodora <teodoraolteanu.25@outlook.com>
2026-05-11 14:08:43 +0300

    fixed the shape and added rotation on z-axis

--- MyShape.c
+++ MyShape.c
@@ -18,7 +18,8 @@ void CALLBACK RotireJos(AUX_EVENTREC* event);
 GLUquadricObj* obj;
 static GLfloat x = 0;
 static GLfloat y = 0;
-static GLfloat alfa = 45.0;
+static GLfloat alfaX = 45.0;
+static GLfloat alfaY = 0.0;
 
 void myinit(void) {
     glClearColor(1.0, 1.0, 1.0, 1.0);
@@ -31,8 +32,10 @@ void CALLBACK MutaStanga(void) { x = x - 10; }
 void CALLBACK MutaDreapta(void) { x = x + 10; }
 void CALLBACK MutaSus(void) { y = y + 10; }
 void CALLBACK MutaJos(void) { y = y - 10; }
-void CALLBACK RotireSus(AUX_EVENTREC* event) { alfa = alfa + 10; }
-void CALLBACK RotireJos(AUX_EVENTREC* event) { alfa = alfa - 10; }
+void CALLBACK RotireSus(AUX_EVENTREC* event) { alfaX = alfaX + 10; }
+void CALLBACK RotireJos(AUX_EVENTREC* event) { alfaX = alfaX - 10; }
+void CALLBACK RotireDreaptaY(void) { alfaY = alfaY + 10; }
+void CALLBACK RotireStangaY(void) { alfaY = alfaY - 10; }
 
 
 //void CALLBACK display(void)
@@ -98,6 +101,109 @@ void CALLBACK RotireJos(AUX_EVENTREC* event) { alfa = alfa - 10; }
 //}
 
 
+//void CALLBACK display(void)
+//{
+//    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+//    obj = gluNewQuadric();
+//    glLoadIdentity();
+//
+//    glTranslatef(x, y, 0.0);
+//    glRotatef(alfa, 1.0, 0.0, 0.0);
+//
+//    gluQuadricDrawStyle(obj, GLU_FILL);
+//    gluQuadricNormals(obj, GLU_SMOOTH);
+//
+//    glPushMatrix();
+//    glColor3f(0.6f, 0.6f, 0.7f);
+//    glTranslatef(0, 0, -2);
+//    gluCylinder(obj, 1.5, 1.5, 4, 32, 1);
+//    glPopMatrix();
+//
+//    glPushMatrix();
+//    glColor3f(0.7f, 0.7f, 0.8f);
+//    glTranslatef(0, 0, -62);
+//    gluCylinder(obj, 30, 30, 10, 64, 10);
+//    glTranslatef(0, 0, 10);
+//    gluCylinder(obj, 30, 1.5, 50, 64, 10);
+//    glTranslatef(0, 0, -10);
+//    gluDisk(obj, 0, 30, 64, 1);
+//
+//    glColor3f(0.4f, 0.2f, 0.0f);
+//    glTranslatef(0, 0, -5);
+//    gluDisk(obj, 0, 45, 64, 1);
+//    gluCylinder(obj, 45, 45, 5, 64, 1);
+//    glTranslatef(0, 0, 5);
+//    gluDisk(obj, 0, 45, 64, 1);
+//    glPopMatrix();
+//
+//    glPushMatrix();
+//    glColor3f(0.7f, 0.7f, 0.8f);
+//    glTranslatef(0, 0, 2);
+//    gluCylinder(obj, 1.5, 30, 50, 64, 10);
+//    glTranslatef(0, 0, 50);
+//    gluCylinder(obj, 30, 30, 10, 64, 10);
+//    glTranslatef(0, 0, 10);
+//    gluDisk(obj, 0, 30, 64, 1);
+//
+//    glColor3f(0.4f, 0.2f, 0.0f);
+//    gluDisk(obj, 0, 45, 64, 1);
+//    gluCylinder(obj, 45, 45, 5, 64, 1);
+//    glTranslatef(0, 0, 5);
+//    gluDisk(obj, 0, 45, 64, 1);
+//    glPopMatrix();
+//
+//    glColor3f(0.4f, 0.2f, 0.0f);
+//
+//    glPushMatrix();
+//    glTranslatef(-40, 0, -65);
+//    gluCylinder(obj, 2, 2, 130, 20, 1);
+//    glPushMatrix();
+//    gluSphere(obj, 5, 20, 20);
+//    glPopMatrix();
+//    glPushMatrix();
+//    glTranslatef(0, 0, 130);
+//    gluSphere(obj, 5, 20, 20);
+//    glPopMatrix();
+//    glPopMatrix();
+//
+//    glPushMatrix();
+//    glTranslatef(40, 0, -65);
+//    gluCylinder(obj, 2, 2, 130, 20, 1);
+//    glPushMatrix();
+//    gluSphere(obj, 5, 20, 20);
+//    glPopMatrix();
+//    glPushMatrix();
+//    glTranslatef(0, 0, 130);
+//    gluSphere(obj, 5, 20, 20);
+//    glPopMatrix();
+//    glPopMatrix();
+//
+//
+//    glPushMatrix();
+//    glTranslatef(0, 40, -65);
+//    gluCylinder(obj, 2, 2, 130, 20, 1);
+//    gluSphere(obj, 5, 20, 20); 
+//    glPushMatrix();
+//    glTranslatef(0, 0, 130);
+//    gluSphere(obj, 5, 20, 20);
+//    glPopMatrix();
+//    glPopMatrix();
+//
+//
+//    glPushMatrix();
+//    glTranslatef(0, -40, -65);
+//    gluCylinder(obj, 2, 2, 130, 20, 1);
+//    gluSphere(obj, 5, 20, 20);
+//    glPushMatrix();
+//    glTranslatef(0, 0, 130);
+//    gluSphere(obj, 5, 20, 20); 
+//    glPopMatrix();
+//    glPopMatrix();
+//
+//    auxSwapBuffers();
+//    gluDeleteQuadric(obj);
+//}
+
 void CALLBACK display(void)
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -105,26 +211,30 @@ void CALLBACK display(void)
     glLoadIdentity();
 
     glTranslatef(x, y, 0.0);
-    glRotatef(alfa, 1.0, 0.0, 0.0);
+   // glRotatef(alfaY, 0.0, 1.0, 0.0);
+    glRotatef(alfaX, 1.0, 0.0, 0.0);
+    glRotatef(alfaY, 0.0, 0.0, 1.0);
 
     gluQuadricDrawStyle(obj, GLU_FILL);
     gluQuadricNormals(obj, GLU_SMOOTH);
 
+    // GATUL CLEPSIDREI (CILINDRUL MIC CENTRAL)
     glPushMatrix();
     glColor3f(0.6f, 0.6f, 0.7f);
     glTranslatef(0, 0, -2);
-    gluCylinder(obj, 1.5, 1.5, 4, 32, 1);
+    gluCylinder(obj, 2.5, 2.5, 5, 32, 5);
     glPopMatrix();
 
+    // CORPUL DE SUS (STICLA CONICA + BAZA LEMN SUS)
     glPushMatrix();
     glColor3f(0.7f, 0.7f, 0.8f);
     glTranslatef(0, 0, -62);
-    gluCylinder(obj, 30, 30, 10, 64, 10);
-    glTranslatef(0, 0, 10);
-    gluCylinder(obj, 30, 1.5, 50, 64, 10);
-    glTranslatef(0, 0, -10);
+    gluCylinder(obj, 30, 30, 30, 64, 10);
+    glTranslatef(0, 0, 30);
+    gluCylinder(obj, 30, 2.5, 30, 64, 10);
+    glTranslatef(0, 0, -30);
     gluDisk(obj, 0, 30, 64, 1);
-
+    
     glColor3f(0.4f, 0.2f, 0.0f);
     glTranslatef(0, 0, -5);
     gluDisk(obj, 0, 45, 64, 1);
@@ -132,14 +242,15 @@ void CALLBACK display(void)
     glTranslatef(0, 0, 5);
     gluDisk(obj, 0, 45, 64, 1);
     glPopMatrix();
-
+    
+    // CORPUL DE JOS (STICLA CONICA + BAZA LEMN JOS)
     glPushMatrix();
     glColor3f(0.7f, 0.7f, 0.8f);
     glTranslatef(0, 0, 2);
-    gluCylinder(obj, 1.5, 30, 50, 64, 10);
-    glTranslatef(0, 0, 50);
-    gluCylinder(obj, 30, 30, 10, 64, 10);
-    glTranslatef(0, 0, 10);
+    gluCylinder(obj, 2.5, 30, 30, 64, 10);
+    glTranslatef(0, 0, 30);
+    gluCylinder(obj, 30, 30, 30, 64, 10);
+    glTranslatef(0, 0, 30);
     gluDisk(obj, 0, 30, 64, 1);
 
     glColor3f(0.4f, 0.2f, 0.0f);
@@ -151,6 +262,7 @@ void CALLBACK display(void)
 
     glColor3f(0.4f, 0.2f, 0.0f);
 
+    // TIJA LATERALA 1 (STÂNGA) CU BILE LA CAPETE
     glPushMatrix();
     glTranslatef(-40, 0, -65);
     gluCylinder(obj, 2, 2, 130, 20, 1);
@@ -163,12 +275,35 @@ void CALLBACK display(void)
     glPopMatrix();
     glPopMatrix();
 
+    // TIJA LATERALA 2 (DREAPTA) CU BILE LA CAPETE
     glPushMatrix();
     glTranslatef(40, 0, -65);
     gluCylinder(obj, 2, 2, 130, 20, 1);
     glPushMatrix();
     gluSphere(obj, 5, 20, 20);
     glPopMatrix();
+    glPushMatrix();
+    glTranslatef(0, 0, 130);
+    gluSphere(obj, 5, 20, 20);
+    glPopMatrix();
+    glPopMatrix();
+
+    // TIJA LATERALA 3 (FAȚĂ) CU BILE LA CAPETE
+    glPushMatrix();
+    glTranslatef(0, 40, -65);
+    gluCylinder(obj, 2, 2, 130, 20, 1);
+    gluSphere(obj, 5, 20, 20);
+    glPushMatrix();
+    glTranslatef(0, 0, 130);
+    gluSphere(obj, 5, 20, 20);
+    glPopMatrix();
+    glPopMatrix();
+
+    // TIJA LATERALA 4 (SPATE) CU BILE LA CAPETE
+    glPushMatrix();
+    glTranslatef(0, -40, -65);
+    gluCylinder(obj, 2, 2, 130, 20, 1);
+    gluSphere(obj, 5, 20, 20);
     glPushMatrix();
     glTranslatef(0, 0, 130);
     gluSphere(obj, 5, 20, 20);
@@ -208,6 +343,9 @@ int main(int argc, char** argv)
 
     auxMouseFunc(AUX_LEFTBUTTON, AUX_MOUSEDOWN, RotireSus);
     auxMouseFunc(AUX_RIGHTBUTTON, AUX_MOUSEDOWN, RotireJos);
+
+    auxKeyFunc(AUX_a, RotireStangaY);
+    auxKeyFunc(AUX_d, RotireDreaptaY);
 
     auxReshapeFunc(myReshape);
     auxMainLoop(display);
